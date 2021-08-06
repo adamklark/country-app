ba13b1323158e76093fa3b30baa129a665ff3287
adamklark <adamvd9@gmail.com>
2021-08-06 20:46:52 +0200

    add-route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello react</h1>
-    </div>
-  );
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Notfound from './pages/Notfound';
+const App = () => {
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route path = "/" exact component={Home} />
+        <Route path = "/a-propos" exact component={About} />
+        <Route component={Notfound} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 const Home = () => {
     return(
         <div className="home">
-            
+            <h1>Accueil</h1>
         </div>
     )
 }
